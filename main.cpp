684139aea1fde23750ef0eac96d08de1fe52417d
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2021-03-09 01:20:06 -0800

    Pragma once does't work, put everything into one file, now it works.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
-#include "dist_sddmm_kernel.hpp"
 #include <bcl/bcl.hpp>
 #include <bcl/containers/DMatrix.hpp>
-
 
 using namespace std;
 
@@ -26,6 +24,39 @@ struct coord_sort_key
     }
 };
 
+std::vector<double> fetch_row(BCL::DMatrix<double> &A, size_t abs_row) {
+    size_t tile_num =  abs_row / A.tile_shape()[0];
+    size_t row_num  = abs_row % A.tile_shape()[0];
+    // printf("Getting row %lu, which should be row %lu within tile %lu:\n",
+    //        i, row_num, tile_num);
+    return(A.get_tile_row(tile_num, 0, row_num));
+}
+
+void SDDMM_column_dist( vector<pair<size_t, size_t>> &coordinates, 
+            BCL::DMatrix<double> &A,
+            BCL::DMatrix<double> &B,
+            double* result 
+            ) {
+    
+    // We assume that the local coordinates are sorted by row so we can
+    // just call this kernel repeatedly
+
+    int currentRow = -1;
+
+    vector<double> Btile = B.get_tile(BCL::rank(), 0);
+
+    for(int i = 0; i < coordinates.size(); i++) {
+        vector<double> Arow;
+        if (coordinates[i].first != currentRow) {
+            if(coordinates[i].first > currentRow) {
+                cout << "Not ordered correctly." << endl;
+            }
+            Arow = fetch_row(A, coordinates[i].first); // Could do asynchronously, perhaps?
+            double* Brow = Btile.data() + B.shape()[1] * coordinates[i].second;
+            result[i] = cblas_ddot(B.shape()[1], Arow.data(), 1, Brow, 1);
+        }
+    }
+}
 
 int main(int argc, char** argv) {
     cout << "Starting kernel" << endl;
---
+++ modules.sh
@@ -0,0 +1,10 @@
+module load cmake
+module load openmpi
+
+module swap PrgEnv-intel PrgEnv-gnu
+module load cray-shmem
+module load tbb
+export MKLROOT=/opt/intel/compilers_and_libraries_2019.3.199/linux/mkl
+
+
+
