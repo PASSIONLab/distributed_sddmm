3067e497009d92658f62e23741fccfb91eee18ae
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2021-06-29 00:54:47 -0700

    The algorithm works for a single layer... will it work for multiple?

--- test_harness.cpp
+++ test_harness.cpp
@@ -17,11 +17,9 @@ int main(int argc, char** argv) {
     //FusedStandardKernel fused_local_ops;
 
     Sparse15D_MDense_Shift_Striped* d_ops = new Sparse15D_MDense_Shift_Striped(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &local_ops);
-    //Sparse15D_MDense_Shift* d_ops = new Sparse15D_MDense_Shift(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &local_ops);
+    // Sparse15D_MDense_Shift* d_ops = new Sparse15D_MDense_Shift(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &local_ops);
 
     //Sparse25D_MDense_Nostage* d_ops = new Sparse25D_MDense_Nostage(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &local_ops);
-
-    //Sparse15D_MDense_Shift* d_ops = new Sparse15D_MDense_Shift(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &local_ops);
 
     //Sparse25D* d_ops = new Sparse25D(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &local_ops);
 
@@ -32,7 +30,7 @@ int main(int argc, char** argv) {
     //Distributed_ALS* x = new Distributed_ALS(d_ops, d_ops->grid->GetLayerWorld(), true, false);
 
     d_ops->reset_performance_timers();
-    x->run_cg(1);
+    x->run_cg(2);
     d_ops->print_performance_statistics();
 
     MPI_Finalize();
