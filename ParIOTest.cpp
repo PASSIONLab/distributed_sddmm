f951f443bf57b803d42974f1df266dac094c216e
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2021-10-11 23:58:27 -0700

    Ready to fix the bugs uncovered by the experiment.

--- ParIOTest.cpp
+++ ParIOTest.cpp
@@ -57,40 +57,21 @@ int main(int argc, char* argv[])
 	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
 
-	if(argc < 3)
-	{
-		if(myrank == 0)
-		{
-			cout << "Usage: ./ParIOTest <MatrixA> <MatrixB_general>" << endl;
-			cout << "<MatrixA> is an absolute address, and file should be in Matrix Market format" << endl;
-			cout << "<MatrixB_general> is an absolute address, file is in general triples format (MCL calls this label input)" << endl;			
-			
-		}
-		MPI_Finalize(); 
-		return -1;
-	}				
-	{
-		string Aname(argv[1]);	
-		string Bname(argv[2]);
-	
-        	PSpMat<double>::MPI_DCCols A, B;
-		
-        	A.ParallelReadMM(Aname, true, maximum<double>());
+	{	
+		string Bname(argv[1]);	
+		string original;
+		original = Bname;
+
+		PSpMat<double>::MPI_DCCols B;
 		FullyDistVec<int64_t, array<char, MAXVERTNAME> > perm = B.ReadGeneralizedTuples(Bname, maximum<double>());
 
-
-		if (A == B)
-		{
-			SpParHelper::Print("Parallel Matrix Market I/O working correctly\n");
+		for(int i = 0; i < 4; i++) {
+			Bname.pop_back();
 		}
-		else
-		{
-			SpParHelper::Print("ERROR in Parallel Matrix Market I/O");
-			A.ParallelWriteMM("A_Error.mtx", true);
-			B.ParallelWriteMM("B_Error.mtx", true);
-		}
+		Bname += "-permuted.mtx";
 
-		perm.ParallelWrite("PermutationVec.mtx", 1, StdArrayReadSaveHandler(), true);
+		B.ParallelWriteMM(Bname, true);
+		cout << "Permuted " << original << "!" << endl;
 	}
 	MPI_Finalize();
 	return 0;
